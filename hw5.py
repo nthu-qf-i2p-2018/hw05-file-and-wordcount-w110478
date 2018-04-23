d189fd3cce5b533b48f07735e6462bb14ed62115
w110478 <37034558+w110478@users.noreply.github.com>
2018-04-23 23:32:55 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,52 +1,68 @@
 # -*- coding: utf-8 -*-
-import ...
-
+from collections import Counter
+import csv
+import string
+import json
+import pickle
 
 def main(filename):
-    # read file into lines
-    lines = ...
-
-    # declare a word list
+    f=open(filename)
+    lines = f.readlines()
     all_words = []
-
-    # extract all words from lines
     for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
+        words = line.split()
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
+            word = word.strip('!')
+            word = word.strip('"')
+            word = word.strip('#')
+            word = word.strip('$')
+            word = word.strip('%')
+            word = word.strip('&')
+            word = word.strip('\\')
+            word = word.strip('\'')
+            word = word.strip('(')
+            word = word.strip(')')
+            word = word.strip('*')
+            word = word.strip('+')
+            word = word.strip(',')
+            word = word.strip('-')
+            word = word.strip('/')
+            word = word.strip(':')
+            word = word.strip(';')
+            word = word.strip('<')
+            word = word.strip('=')
+            word = word.strip('>')
+            word = word.strip('?')
+            word = word.strip('@')
+            word = word.strip('[')
+            word = word.strip(']')
+            word = word.strip('^')
+            word = word.strip('_')
+            word = word.strip('{')
+            word = word.strip('|')
+            word = word.strip('}')
+            word = word.strip('~')
+            word = word.strip('.')
             if word:
-                # append the word to "all_words" list
-                all_words...
+                all_words.append(word)
+    counter = Counter()
+    counter.update(all_words)
 
-    # compute word count from all_words
-    counter = ...
-
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
+    with open('wordcount.csv','w') as csv_file:
+        writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
-
-    # dump to a json file named "wordcount.json"
-    ...
-
-    # BONUS: dump to a pickle file named "wordcount.pkl"
-    # hint: dump the Counter object directly
-
+        writer.writerows(counter.most_common())
+        csv_file.close()
+    with open('wordcount.json','w') as json_file:
+        json.dump(counter.most_common(),json_file)
+        writer.writerow(['word', 'count'])
+        writer.writerows(counter.most_common())
+        json_file.close()
+    with open('wordcount.pkl','wb') as pkl_file:
+        pickle.dump(counter,pkl_file)
+        writer.writerow(['word', 'count'])
+        writer.writerows(counter.most_common())
+        pkl_file.close()
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
